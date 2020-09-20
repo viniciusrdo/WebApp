1ae6290c7e8d32fbbc445db44c1f9041f00c1eab
Vinicius <vinioliveira015@gmail.com>
2020-09-20 20:51:16 -0300

    This class is responsible for "run" the application, and start the SpringBoot resources.

---
+++ UnisoApplication.java
@@ -0,0 +1,13 @@
+package br.uniso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnisoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnisoApplication.class, args);
+    }
+
+}
